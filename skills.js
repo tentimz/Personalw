3401594b7f23926d7d4236cc70ecf40c21820060
tentimz <68075707+tentimz@users.noreply.github.com>
2023-10-16 13:56:48 +0100

    Add files via upload

--- skills.js
+++ skills.js
@@ -8,3 +8,24 @@ function showImage() {
   fileContainer.innerHTML =
     '<img src=" /Users/ftenfati/Desktop/MyPWS/BusinessAnalyst.png" alt="Image"></iframe>';
 }
+
+    
+//
+
+// file.js
+/*function openPopup() {
+  var url = "Problem Solving.html";
+  var popupWindow = window.open(url, "Popup", "width=500,height=300");
+  if (popupWindow) {
+      popupWindow.focus();
+  }
+  return false;
+}*/
+
+function openPopup(url) {
+  var popupWindow = window.open(url, "Popup", "width=500,height=300");
+  if (popupWindow) {
+    popupWindow.focus();
+  }
+  return false;
+}
