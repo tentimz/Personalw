f48e8c16fd7df06c16b1d825015956e7f54e2ea6
tentimz <68075707+tentimz@users.noreply.github.com>
2023-11-29 18:24:26 +0100

    Add files via upload

--- skills.js
+++ skills.js
@@ -9,23 +9,74 @@ function showImage() {
     '<img src=" /Users/ftenfati/Desktop/MyPWS/BusinessAnalyst.png" alt="Image"></iframe>';
 }
 
-    
-//
 
-// file.js
-/*function openPopup() {
-  var url = "Problem Solving.html";
-  var popupWindow = window.open(url, "Popup", "width=500,height=300");
-  if (popupWindow) {
-      popupWindow.focus();
-  }
-  return false;
-}*/
+document.addEventListener("DOMContentLoaded", function() {
 
-function openPopup(url) {
-  var popupWindow = window.open(url, "Popup", "width=500,height=300");
-  if (popupWindow) {
-    popupWindow.focus();
-  }
-  return false;
-}
+  document.getElementById('square1').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'https://salesforce.vidyard.com/watch/PjNcmecuqcYKH2cJsfLEAy?_ga=2.175975522.1748971924.1698078343-199303864.1695985956&_gl=1*10xemea*_ga*MTk5MzAzODY0LjE2OTU5ODU9IN';
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById('square2').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'sfdatabase.html';
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById('square3').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'https://trailhead.salesforce.com/content/learn/superbadges/superbadge_apex';
+  });
+});
+document.addEventListener("DOMContentLoaded", function() {
+ 
+  document.getElementById('square4').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'https://trailhead.salesforce.com/content/learn/modules/lex_dev_visualforce';
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+  document.getElementById('square5').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'https://trailhead.salesforce.com/content/learn/modules/soql-for-admins?trailmix_creator_id=filholmc&trailmix_slug=sql-soql-and-sosl';
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+ 
+  document.getElementById('square6').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'https://trailhead.salesforce.com/content/learn/modules/pw-api-basics';
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+  document.getElementById('square7').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'https://trailhead.salesforce.com/content/learn/trails/discover-sales-cloud';
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+  document.getElementById('square8').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'webdev.html';
+});
+});
+document.addEventListener("DOMContentLoaded", function() {
+  
+  document.getElementById('square9').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'https://trailhead.salesforce.com/content/learn/modules/lex_implementation_user_setup_mgmt?trailmix_creator_id=adhawan1&trailmix_slug=security-permission-set-profiles-roles';
+});
+});
