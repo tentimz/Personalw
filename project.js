8143f70fe45b81dbee8f603c88cdb0eba3fd45f5
tentimz <68075707+tentimz@users.noreply.github.com>
2024-10-03 17:50:44 +0100

    Add files via upload

---
+++ project.js
@@ -0,0 +1,16 @@
+// Add a click event listener to each square
+document.getElementById("square1").addEventListener("click", function() {
+    alert("You clicked Square 1!");
+});
+
+document.getElementById("square2").addEventListener("click", function() {
+    alert("You clicked Square 2!");
+});
+
+document.getElementById("square3").addEventListener("click", function() {
+    alert("You clicked Square 3!");
+});
+
+document.getElementById("square4").addEventListener("click", function() {
+    alert("You clicked Square 4!");
+});
